9a066c04ed8767ccfba23070f2018bd4d49d217b
Git-Shreyasks <shreyasgavi@gmail.com>
2026-03-24 12:22:30 -0500

    Initial commit: optical-network-route-protection

---
+++ src/algorithms/suurballe.py
@@ -0,0 +1,66 @@
+def dijkstra(graph, initial, destination):
+    size = len(graph)
+    visited = {initial: 0}
+    path = {}
+    nodes = {x for x in range(size)}
+
+    while nodes:
+        minNode = None
+        for node in nodes:
+            if node in visited:
+                if minNode is None:
+                    minNode = node
+                elif visited[node] < visited[minNode]:
+                    minNode = node
+        if minNode is None:
+            break
+
+        nodes.remove(minNode)
+        currentWeight = visited[minNode]
+
+        for i in range(size):
+            if graph[minNode][i] >= 0:
+                weight = currentWeight + graph[minNode][i]
+                if i not in visited or weight < visited[i]:
+                    visited[i] = weight
+                    path[i] = minNode
+
+    path1 = {}
+    current = destination
+    while current != initial:
+        path1[current] = path[current]
+        current = path[current]
+
+    return visited, path1
+
+def suurballe(graph, initial, destination):
+    visited, path1 = dijkstra(graph, initial, destination)
+    # Update costs:
+    size = len(graph)
+    original = [row[:] for row in graph]
+    for i in range(size):
+        for j in range(size):
+            if graph[i][j] >= 0:
+                graph[i][j] += visited[i] - visited[j]
+    # Create residual graph
+    # Remove edges directed into s and reversing the direction of the zero length edges along path1
+    for j, i in path1.items():
+        graph[j][i] = graph[i][j]
+        graph[i][j] = -1
+    # Find second path
+    visited, path2 = dijkstra(graph, initial, destination)
+    # Discard the reversed edges of path2 from both paths.
+    js = list(path2.keys())  # Create a list of keys to iterate over
+    for j in js:
+        i = path2[j]
+        if i in path1:
+            if path1[i] == j:
+                del path1[i]
+                del path2[j]
+    # Remaining edges:
+    result = [[-1 for x in range(size)] for x in range(size)]
+    for j, i in path1.items():
+        result[i][j] = original[i][j]
+    for j, i in path2.items():
+        result[i][j] = original[i][j]
+    return result
---
+++ src/simulations/nerwork_simulator.py
@@ -0,0 +1,136 @@
+import sys
+import pydot
+from suurballe import *
+
+def print_graph(graph, fileName, result=None):
+    pydotGraph = pydot.Dot(graph_type='graph')
+    size = len(graph)
+    for i in range(size):
+        for j in range(i, size):
+            if graph[i][j] >= 0:
+                edge = pydot.Edge(str(i+1), str(j+1))
+                edge.set_label(str(graph[i][j]))
+                if result is not None:
+                    if result[i][j] >= 0:
+                        edge.set_color('red')
+                pydotGraph.add_edge(edge)
+    pydotGraph.write_png(fileName)
+
+def print_digraph(digraph, fileName):
+    pydotDigraph = pydot.Dot(graph_type='digraph')
+    size = len(digraph)
+    for i in range(size):
+        for j in range(size):
+            if digraph[i][j] >= 0:
+                edge = pydot.Edge(str(i+1), str(j+1))
+                edge.set_label(str(digraph[i][j]))
+                pydotDigraph.add_edge(edge)
+    pydotDigraph.write_png(fileName);
+
+def read_graph(inputFileName):
+    inputFile = open(inputFileName)
+    line = inputFile.readline()
+    size = int(line)
+    graph = [[-1 for x in range(size)] for x in range(size)]
+    for line in inputFile:
+        values = line.split()
+        v1 = int(values[0]) - 1
+        v2 = int(values[1]) - 1
+        weight = 1
+        if len(values) > 2:
+            weight = float(values[2])
+        if v1 < v2:
+            graph[v1][v2] = weight
+        else:
+            graph[v2][v1] = weight
+    return graph
+
+def graph_to_digraph(graph):
+    size = len(graph)
+    digraph = [[-1 for x in range(size)] for x in range(size)]
+    for i in range(size):
+        for j in range(i, size):
+            digraph[i][j] = graph[i][j]
+            digraph[j][i] = graph[i][j]
+    return digraph
+
+def transform_digraph(digraph):
+    size = len(digraph)
+    newSize = size * 2
+    transformed = [[-1 for x in range(newSize)] for x in range(newSize)]
+    for i in range(size):
+        for j in range(size):
+            if digraph[i][j] >= 0:
+                transformed[2*i][2*j+1] = digraph[i][j]
+                transformed[2*i+1][2*j] = digraph[i][j]
+    return transformed
+
+def transform_unique_vertex(digraph):
+    size = len(digraph)
+    newSize = size * 2
+    transformed = [[-1 for x in range(newSize)] for x in range(newSize)]
+    for i in range(size):
+        for j in range(size):
+            if digraph[i][j] >= 0:
+                    transformed[2*i+1][2*j] = digraph[i][j]
+        transformed[2*i][2*i+1] = 0
+    return transformed
+
+def detransform(paths):
+    size = len(paths)
+    newSize = int(size / 4)  # Ensure that the new size is an integer
+    result = [[-1 for x in range(newSize)] for x in range(newSize)]
+    
+    for i in range(size):
+        for j in range(size):
+            if paths[i][j] >= 0:
+                result[int(i / 4)][int(j / 4)] = paths[i][j]  # Ensure the indices are integers
+    
+    return result
+
+def print_lp_data(graph, fileName):
+    output = open(fileName, 'w');
+    output.write("data;\n\nset N :=\n");
+    size = len(graph)
+    for i in range(size):
+        output.write(str(i+1))
+        if i != size-1:
+            output.write(',')
+    output.write(';\n\nparam: A: a c :=\n')
+    for i in range(size):
+        for j in range(size):
+            if graph[i][j] >= 0:
+                output.write(str(i+1) + ',' + str(j+1) + ' ' + str(graph[i][j]) + ' 1\n')
+    output.write(';\n\nparam r :=\n')
+    for i in range(size):
+        if i == 0:
+            output.write(str(i+1) + ' 2\n')
+        elif i == size-1:
+            output.write(str(i+1) + ' -2\n')
+        else:
+            output.write(str(i+1) + ' 0\n')
+    output.write(';\n\nend;')
+    output.close()
+
+if __name__ == '__main__':
+    graph = read_graph(sys.argv[1])
+    print_graph(graph, 'graph1_original.png')
+    digraph = graph_to_digraph(graph)
+    print_graph(digraph, 'graph2_digraph.png')
+    transformed = transform_digraph(digraph)
+    print_graph(transformed, 'graph3_transformed.png')
+    transformed2 = transform_unique_vertex(transformed)
+    transformed3 = [row[:] for row in transformed2]
+    print_graph(transformed2, 'graph4_transformed2.png')
+    print_lp_data(transformed2, 'minimum_cost_flow.dat')
+    initial = 1
+    destination1 = len(transformed2) - 4
+    destination2 = len(transformed2) - 2
+    resultT1 = suurballe(transformed2, initial, destination1)
+    print_digraph(resultT1, 'graph5_path1.png')
+    resultT2 = suurballe(transformed3, initial, destination2)
+    print_digraph(resultT2, 'graph6_path2.png')
+    result1 = detransform(resultT1)
+    result2 = detransform(resultT2)
+    print_graph(graph, 'graph7_result1.png', result1)
+    print_graph(graph, 'graph8_result2.png', result2)
